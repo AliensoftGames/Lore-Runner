ba7475626c07d06619661c2e74ccb27f41a8d3e8
AliensoftGames <teosmekeru10@gmail.com>
2023-11-25 17:22:46 +0200

    PlayerUpdate

--- Lore Runner/Assets/Scripts/PlayerMovement.cs
+++ Lore Runner/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -7,29 +8,41 @@ public class PlayerMovement : MonoBehaviour
     public float TurningSpeed;
     public GameObject PlayerMesh;
     public Camera Playercam;
+    public float JumpForce = 0;
+    bool IsJumping = false;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    int Offset = 0;
+    float Offset = 0;
     // Update is called once per frame
     void Update()
     {
-        int axis = (int)Input.GetAxis("Horizontal");
-        if(axis == 1)
+        if(Input.GetKeyUp(KeyCode.A))
         {
-            Offset = 5;
+            Offset -= 5;
         }
-        else if(axis == -1)
+        else if(Input.GetKeyUp(KeyCode.D))
         {
-            Offset = -5;
+            Offset += 5;
         }
-        else
+        if (Input.GetButtonDown("Jump") && IsJumping)
         {
-            Offset = 0;
+            PlayerMesh.GetComponent<Rigidbody>().AddForce(Vector3.up * JumpForce * 100 * Time.deltaTime, ForceMode.Impulse);
+            
         }
+        Offset = Mathf.Clamp(Offset, -5, 5);
         PlayerMesh.transform.position = Vector3.Lerp(PlayerMesh.transform.position, new Vector3(Offset, PlayerMesh.transform.position.y, PlayerMesh.transform.position.z), TurningSpeed * Time.deltaTime * 100);
         Playercam.transform.position = Vector3.Lerp(Playercam.transform.position, new Vector3(Offset, Playercam.transform.position.y, Playercam.transform.position.z), TurningSpeed * Time.deltaTime * 100);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        PlayerMesh.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        IsJumping = true;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        IsJumping = false;
     }
 }
